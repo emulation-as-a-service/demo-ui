08093373075a6acbbef61372970dd6eca86faa22
Klaus Rechert <klaus@openslx.com>
2019-07-17 13:47:26 +0200

    Revert "enable networks to start dhcpd"
    This reverts commit fab86849881c88a488be6f9171c978dab1e656c0.

--- eaasi-public-portal/src/app/modules/emulator/start.ctrl.js
+++ eaasi-public-portal/src/app/modules/emulator/start.ctrl.js
@@ -52,7 +52,7 @@ module.exports = ['$rootScope', '$uibModal', '$scope', '$http', '$sce', '$state'
 
             let params = {};
             if (chosenEnv) {
-                if (true || chosenEnv.connectEnvs)
+                if (chosenEnv.connectEnvs)
                     params.enableNetwork = true;
                     
                 if (chosenEnv.localServerMode) {
