928d817079e0b41a46c7006a87ecfeca537e5601
Klaus Rechert <klaus@openslx.com>
2021-11-18 19:49:33 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,9 +7,6 @@ git submodule init
 git submodule update
 cd ..
 
-cd landing-page
-npm install && \
-npm run build && \
-cd ../eaas-frontend-admin && \
+cd eaas-frontend-admin && \
 npm install && \
 npm run build
