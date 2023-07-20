d374d3868cacd3c7b9b2f730b6ffc8d45dc7f7ca
Jurek <jurek@openslx.com>
2023-07-20 13:49:37 +0200

    increase timeout and allow 1 retry

--- playwright-tests/playwright.config.js
+++ playwright-tests/playwright.config.js
@@ -23,7 +23,7 @@ let config = {
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only  FIXME Testing = no retries, Finalized CI should retry!*/
-    retries: process.env.CI ? 0 : 0,
+    retries: process.env.CI ? 1 : 0,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
